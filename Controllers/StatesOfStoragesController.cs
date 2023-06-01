14302d0a64aad8d5fe26c8d513228c4e613d606f
Vladimir-112358 <Vaganov11235813213455@gmail.com>
2023-06-01 14:13:16 +0300

    added methods GetAll from controllers

--- Controllers/StatesOfStoragesController.cs
+++ Controllers/StatesOfStoragesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EntityFCAndWebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class StatesOfStoragesController : ControllerBase
     {
